d59309d2f08eff18b425d6850aad5dd9e451fdaa
bhanu thareja <jimmythareja@icloud.com>
2017-08-31 18:02:20 +0300

    added gitignore

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.use(express.static(path.join(__dirname, 'www')));
 
 require('./api/db');
 
-console.log(process.env.NODE_ENV);
-
 app.listen(3000, function(){
   console.log('listening to port 3000');
 })
